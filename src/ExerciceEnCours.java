db6d56faaa7f44897993c0ba2ef7a13b83922dd0
Desandère <qdesand@gmail.com>
2025-04-11 10:26:35 +0200

    tp3Exo1

--- src/ExerciceEnCours.java
+++ src/ExerciceEnCours.java
@@ -1,9 +1,38 @@
+import java.util.Arrays;
 
 public class ExerciceEnCours {
 
 	public static void main(String[] args) {
-		System.out.println("Helllooooo boiis yoyoyo ahahahaouiiiiiii");
-
+		sortNegativePositiveArray();
 	}
+
+	private static void sortNegativePositiveArray() {
+		int tailleTableauPositifNul = 0, tailleTableauNegatif = 0;
+		double[] tableau = {1.2, 5.6, 0, -2};
+		for(int i = 0; i < tableau.length ; i++) {
+			if (tableau[i] < 0) {
+				tailleTableauNegatif++;
+			}
+			else {
+				tailleTableauPositifNul++;
+			}
+		}
+		double[] tableau1 = new double[tailleTableauNegatif];
+		double[] tableau2 = new double[tailleTableauPositifNul];
+		int compteur1 = 0, compteur2 = 0;
+		for(int i = 0; i < tableau.length ; i++) {
+			if (tableau[i] < 0) {
+				tableau1[compteur1] = tableau[i];
+				compteur1++;
+			}
+			else {
+				tableau2[compteur2] = tableau[i];
+				compteur2++;
+			}
+		}
+		System.out.println(tailleTableauNegatif+" "+Arrays.toString(tableau1)+" "
+				+tailleTableauPositifNul+" "+Arrays.toString(tableau2));
+	}
+	
 
 }
