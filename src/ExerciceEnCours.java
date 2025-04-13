336708dd87ef72ec00f2ede356c739eed573bc8d
Desandère <qdesand@gmail.com>
2025-04-13 11:27:54 +0200

    yo

--- src/ExerciceEnCours.java
+++ src/ExerciceEnCours.java
@@ -1,38 +1,22 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class ExerciceEnCours {
 
 	public static void main(String[] args) {
-		sortNegativePositiveArray();
-	}
-
-	private static void sortNegativePositiveArray() {
-		int tailleTableauPositifNul = 0, tailleTableauNegatif = 0;
-		double[] tableau = {1.2, 5.6, 0, -2};
-		for(int i = 0; i < tableau.length ; i++) {
-			if (tableau[i] < 0) {
-				tailleTableauNegatif++;
-			}
-			else {
-				tailleTableauPositifNul++;
-			}
-		}
-		double[] tableau1 = new double[tailleTableauNegatif];
-		double[] tableau2 = new double[tailleTableauPositifNul];
-		int compteur1 = 0, compteur2 = 0;
-		for(int i = 0; i < tableau.length ; i++) {
-			if (tableau[i] < 0) {
-				tableau1[compteur1] = tableau[i];
-				compteur1++;
-			}
-			else {
-				tableau2[compteur2] = tableau[i];
-				compteur2++;
-			}
-		}
-		System.out.println(tailleTableauNegatif+" "+Arrays.toString(tableau1)+" "
-				+tailleTableauPositifNul+" "+Arrays.toString(tableau2));
+		Erastothene(50);
 	}
 	
-
+	public static void Erastothene(int n) {
+		final int racinneN = (int)Math.sqrt(n);
+		int nb = 1; 
+		int[][] tab = new int[racinneN][racinneN];
+		for(int i = 0 ; i < racinneN ; i++) {
+			for(int j = 0 ; j < racinneN ; j++) {
+				tab[i][j] = nb;
+				nb++;
+			}
+		}
+		for
+	}
 }
