91d736f61d0a07d2f0eeabab6905cfebf64d64f4
Desandère <qdesand@gmail.com>
2025-04-22 10:27:18 +0200

    documents

--- src/ExerciceEnCours.java
+++ src/ExerciceEnCours.java
@@ -4,19 +4,6 @@ import java.util.Random;
 public class ExerciceEnCours {
 
 	public static void main(String[] args) {
-		Erastothene(50);
-	}
-	
-	public static void Erastothene(int n) {
-		final int racinneN = (int)Math.sqrt(n);
-		int nb = 1; 
-		int[][] tab = new int[racinneN][racinneN];
-		for(int i = 0 ; i < racinneN ; i++) {
-			for(int j = 0 ; j < racinneN ; j++) {
-				tab[i][j] = nb;
-				nb++;
-			}
-		}
-		for
+		
 	}
 }
