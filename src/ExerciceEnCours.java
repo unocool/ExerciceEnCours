c3724eb5c3c0468cf84deb6857c4dece3d0b8152
Desandère <qdesand@gmail.com>
2025-05-14 10:32:21 +0200

    yoyo

--- src/ExerciceEnCours.java
+++ src/ExerciceEnCours.java
@@ -4,6 +4,9 @@ import java.util.Random;
 public class ExerciceEnCours {
 
 	public static void main(String[] args) {
-		
+		ListeDocuments biblio = new ListeDocuments();
+		biblio.ajouterDoc(new Livre("LeSeigneurdesAnneaux", "tolkien", 1000));
+		biblio.ajouterDoc(new Livre("Harrypotter", "rowlin", 1500));
+		biblio.afficherdoc();
 	}
 }
